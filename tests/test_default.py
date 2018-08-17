5dceb313c3bf923cd56153bff6521650a15d1f5b
Leif Denby <leif@denby.eu>
2018-08-17 15:47:32 +0100

    add testing through pytest

---
+++ tests/test_default.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+
+import pytest
+import xarray as xr
+
+
+@pytest.fixture
+def ds_track(request):
+    bin_path = os.environ['OLDPWD']
+    main_path = os.path.join(bin_path, 'main')
+    test_path = os.path.dirname(os.path.abspath(__file__))
+    testdata_path = os.path.join(test_path, "testdata")
+
+    os.chdir(testdata_path)
+
+    base_name = "testdata"
+    arg_str = "{} {} 1 5 {}".format(
+        main_path, base_name, request.param
+    )
+    p = subprocess.Popen(arg_str.split(" "), stderr=subprocess.PIPE,
+                         stdout=subprocess.PIPE)
+
+    stdout, stderr = p.communicate()
+
+    if p.returncode != 0:
+        raise Exception(stdout, stderr)
+
+    fn_out = os.path.join(testdata_path, '{}.out.xy.track.nc'.format(base_name))
+
+    return xr.open_dataset(fn_out, decode_times=False)
+
+@pytest.mark.parametrize("ds_track", ["cloud", ], indirect=["ds_track",])
+def test_one(ds_track):
+    assert ds_track.smcloudid.max() == 6
